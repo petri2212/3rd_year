f5f5928f0df3810b36c9d369aaabe51f8d9b8bd3
petri <a.petrisor@studenti.unibg.it>
2024-09-25 23:25:11 +0100

    First Project
    -MAVEN and LOGGING implementation

---
+++ calcolatoremaven/src/main/java/ingegneriasw/calcolatoremaven/App.java
@@ -0,0 +1,10 @@
+package ingegneriasw.calcolatoremaven;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
