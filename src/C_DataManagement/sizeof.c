186eac7e912899d297c0e715393f6ec3e6a37b9c
Andrei <a.petrisor@studenti.unibg.it>
2024-11-16 08:43:57 +0100

    more info about sizeof

--- src/C_DataManagement/sizeof.c
+++ src/C_DataManagement/sizeof.c
@@ -2,10 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define NUMBER_OF_ELEMENTS 50
+
+
 int main(int argc , char *argv[])
 {
 
-printf("%d\n", sizeof(int));
+//printf("%d\n", sizeof(int));
+int a=5;//4 byte
+
+int arr[50] = {3, 9, 10};// -> int* = 4, 4*3=12 byte
+
+int* arrp = arr;
+
+//printf("%zu\n", sizeof(arr)/sizeof(int)); // --> i can use it to discover the size of an array
+// the operator is going to result a size_t type which value is 8 bytes
+printf("%d\n", sizeof(sizeof(int)));
+
 
 return 0;
 
